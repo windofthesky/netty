29d482f12df92d757915d4e2ad3de5c25ad524e7
Norman Maurer <norman_maurer@apple.com>
2016-05-30 13:40:08 +0200

    Optimize HPACK usage to align more with Netty types and remove heavy object creations. Related to [#3597]
    Motivations:
    
    The HPACK code was not really optimized and written with Netty types in mind. Because of this a lot of garbage was created due heavy object creation.
    
    This was first reported in [#3597] and https://github.com/grpc/grpc-java/issues/1872 .
    
    Modifications:
    
    - Directly use ByteBuf as input and output
    - Make use of ByteProcessor where possible
    - Use AsciiString as this is the only thing we need for our http2 usage
    
    Result:
    
    Less garbage and better usage of Netty apis.

--- codec-http2/src/main/java/io/netty/handler/codec/http2/CharSequenceMap.java
+++ codec-http2/src/main/java/io/netty/handler/codec/http2/CharSequenceMap.java
@@ -18,11 +18,17 @@ package io.netty.handler.codec.http2;
 import io.netty.handler.codec.DefaultHeaders;
 import io.netty.handler.codec.UnsupportedValueConverter;
 import io.netty.handler.codec.ValueConverter;
+import io.netty.util.HashingStrategy;
+import io.netty.util.internal.UnstableApi;
 
 import static io.netty.util.AsciiString.CASE_INSENSITIVE_HASHER;
 import static io.netty.util.AsciiString.CASE_SENSITIVE_HASHER;
 
-final class CharSequenceMap<V> extends DefaultHeaders<CharSequence, V, CharSequenceMap<V>> {
+/**
+ * Internal use only!
+ */
+@UnstableApi
+public final class CharSequenceMap<V> extends DefaultHeaders<CharSequence, V, CharSequenceMap<V>> {
     public CharSequenceMap() {
         this(true);
     }
@@ -33,5 +39,11 @@ final class CharSequenceMap<V> extends DefaultHeaders<CharSequence, V, CharSeque
 
     public CharSequenceMap(boolean caseSensitive, ValueConverter<V> valueConverter) {
         super(caseSensitive ? CASE_SENSITIVE_HASHER : CASE_INSENSITIVE_HASHER, valueConverter);
+    }
+
+    @SuppressWarnings("unchecked")
+    public CharSequenceMap(boolean caseSensitive, ValueConverter<V> valueConverter, int arraySizeHint) {
+        super(caseSensitive ? CASE_SENSITIVE_HASHER : CASE_INSENSITIVE_HASHER, valueConverter,
+                NameValidator.NOT_NULL, arraySizeHint);
     }
 }
